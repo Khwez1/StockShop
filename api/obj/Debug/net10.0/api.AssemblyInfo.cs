e732cf01641df0ce32eb67be8cf28994559d7c29
Khwez1 <khwezimbobo@gmail.com>
2026-03-11 04:37:30 +0200

    render build

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c3f1052f972826937ac2abce3b9aee5663f3e88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e55598ff28046b77dfbe2c314387ae6289936817")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
